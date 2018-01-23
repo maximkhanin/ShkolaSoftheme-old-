afd50fc53facf17788c3b36f47bf62ea2ea14e90
Maxim Khanin <maksimkhanin.kpi@gmail.com>
2018-01-23 23:51:16 +0200

    Initial Commit
    Signed-off-by: Maxim Khanin <maksimkhanin.kpi@gmail.com>

---
+++ CSharpHW/1/HW1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Max";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("{0}", name);
+            Console.ReadKey();
+        }
+    }
+}
