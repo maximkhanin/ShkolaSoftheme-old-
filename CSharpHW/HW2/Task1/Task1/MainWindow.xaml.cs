77ee2f30cc86e6155513c50c140f81faf358fc7a
Maxim Khanin <maksimkhanin.kpi@gmail.com>
2018-02-09 17:46:02 +0200

    HW2 fix

--- CSharpHW/HW2/Task1/Task1/MainWindow.xaml.cs
+++ CSharpHW/HW2/Task1/Task1/MainWindow.xaml.cs
@@ -20,8 +20,7 @@ namespace Task1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string _rbType="sbyte";
-        private string _rbValue = "MaxValue";
+        private string _rbType="";
         public MainWindow()
         {
             InitializeComponent();
@@ -33,15 +32,6 @@ namespace Task1
                 }
                 
             }
-            foreach (UIElement c in layoutStackPanelValues.Children)
-            {
-                if (c is RadioButton)
-                {
-                    ((RadioButton)c).Checked += RadioButtonValue_Checked;
-                }
-
-            }
-            
             button1.Click += Button_Click;
             
         }
@@ -51,140 +41,73 @@ namespace Task1
             _rbType = pressed.Content.ToString();
         }
 
-
-        private void RadioButtonValue_Checked(object sender, RoutedEventArgs e)
-        {
-            RadioButton pressed = (RadioButton)sender;
-            _rbValue = pressed.Content.ToString();
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             switch (_rbType)
             {
+
                 case "sbyte":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = sbyte.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = sbyte.MinValue;
-                    }
+                    maxValue.Content = sbyte.MaxValue.ToString();
+                    minValue.Content = sbyte.MinValue.ToString();
+                    defaultValue.Content = default(sbyte).ToString();
                     break;
                 case "byte":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = byte.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = byte.MinValue;
-                    }
+                    maxValue.Content = byte.MaxValue.ToString();
+                    minValue.Content = byte.MinValue.ToString();
+                    defaultValue.Content = default(byte).ToString();
                     break;
                 case "short":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = short.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = short.MinValue;
-                    }
+                    maxValue.Content = short.MaxValue.ToString();
+                    minValue.Content = short.MinValue.ToString();
+                    defaultValue.Content = default(short).ToString();
                     break;
                 case "ushort":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = ushort.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = ushort.MinValue;
-                    }
+                    maxValue.Content = ushort.MaxValue.ToString();
+                    minValue.Content = ushort.MinValue.ToString();
+                    defaultValue.Content = default(ushort).ToString();
                     break;
                 case "int":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = int.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = int.MinValue;
-                    }
+                    maxValue.Content = int.MaxValue.ToString();
+                    minValue.Content = int.MinValue.ToString();
+                    defaultValue.Content = default(int).ToString();
                     break;
                 case "uint":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = uint.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = uint.MinValue;
-                    }
+                    maxValue.Content = uint.MaxValue.ToString();
+                    minValue.Content = uint.MinValue.ToString();
+                    defaultValue.Content = default(uint).ToString();
                     break;
                 case "long":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = long.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = long.MinValue;
-                    }
+                    maxValue.Content = long.MaxValue.ToString();
+                    minValue.Content = long.MinValue.ToString();
+                    defaultValue.Content = default(long).ToString();
                     break;
                 case "ulong":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = ulong.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = ulong.MinValue;
-                    }
+                    maxValue.Content = ulong.MaxValue.ToString();
+                    minValue.Content = ulong.MinValue.ToString();
+                    defaultValue.Content = default(ulong).ToString();
                     break;
                 case "float":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = float.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = float.MinValue;
-                    }
+                    maxValue.Content = float.MaxValue.ToString();
+                    minValue.Content = float.MinValue.ToString();
+                    defaultValue.Content = default(float).ToString();
                     break;
                 case "double":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = double.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = double.MinValue;
-                    }
+                    maxValue.Content = double.MaxValue.ToString();
+                    minValue.Content = double.MinValue.ToString();
+                    defaultValue.Content = default(double).ToString();
                     break;
                 case "decimal":
-                    if (valueCheck(_rbValue) == true)
-                    {
-                        valueType.Content = decimal.MaxValue;
-                    }
-                    else
-                    {
-                        valueType.Content = decimal.MinValue;
-                    }
+                    maxValue.Content = decimal.MaxValue.ToString();
+                    minValue.Content = decimal.MinValue.ToString();
+                    defaultValue.Content = default(decimal).ToString();
+                    break;
+                default:
+                    MessageBox.Show("");
                     break;
             }
         }
 
-        private bool valueCheck(string value)
-        {
-            if (value == "MaxValue"){
-                return true;
-            }
-            else
-            {
-                return false;
-            }       
-         }
+      
 
     }
 }
