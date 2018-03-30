ae2eb4965d67573862339e360ef0d6b86dc1b4c7
maximkhanin <35745941+maximkhanin@users.noreply.github.com>
2018-03-30 18:56:17 +0300

    HW24

---
+++ CSharpHW/HW24/ConsoleApp1/ConsoleApp1/ThreadParams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zip;
+
+namespace ConsoleApp1
+{
+    class ThreadParams
+    {
+        public string Path { get; set; }
+        public ZipFile ZipFile { get; set; }
+    }
+}
