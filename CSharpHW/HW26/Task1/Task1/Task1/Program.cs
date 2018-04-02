8f057f3dead7ae6c43c03ba5a2820734344923f6
Maxim Khanin <maksimkhanin.kpi@gmail.com>
2018-04-02 17:38:24 +0300

    HW26

---
+++ CSharpHW/HW26/Task1/Task1/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.GetEncoding(1251);
+            Console.OutputEncoding = Encoding.GetEncoding(1251);
+            Console.WriteLine("Input the path to the directory");
+            var path = Console.ReadLine();
+            var replaceText = new ReplaceText("old", "new");
+            replaceText.ReplaceTextInFiles(path);
+            Console.WriteLine("Program is finished");
+        }
+    }
+}
---
+++ CSharpHW/HW26/Task1/Task1/Task1/ReplaceText.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class ReplaceText
+    {
+        private const int MaxDegreeOfParallelism = 5;
+        private readonly string _oldText;
+        private readonly string _newText;
+
+        public ReplaceText(string oldText, string newText)
+        {
+            _oldText = oldText;
+            _newText = newText;
+            var fileStream = File.Create("log.txt");
+            fileStream.Close();      
+        }
+
+        public void ReplaceTextInFiles(string path)
+        {
+            var dirs = Directory.GetDirectories(path);
+            var files = Directory.GetFiles(path, "*.txt");
+
+            Parallel.ForEach(files, x =>
+            {
+                new ParallelOptions { MaxDegreeOfParallelism = MaxDegreeOfParallelism };
+                Console.WriteLine(x);
+                var streamReader = File.OpenText(x);
+                var text = String.Empty;
+                var str = String.Empty;
+                var logFile = String.Empty;
+                while ((str = streamReader.ReadLine())!=null)
+                {
+                    if (str.Contains(_oldText))
+                    {
+                        logFile += "Name of the file: " + x + " String:" + str;
+                        str = str.Replace(_oldText, _newText);
+                        logFile +=" Result:" + str + "\r\n";
+                    }
+                    text += str + "\r\n";
+                }
+                streamReader.Close();
+                File.WriteAllText(x, text);
+                lock (this)
+                {
+                   File.AppendAllText("log.txt", logFile);
+                }  
+            });
+
+            Parallel.ForEach(dirs, x =>
+            {
+                new ParallelOptions { MaxDegreeOfParallelism = MaxDegreeOfParallelism };
+                ReplaceTextInFiles(x);
+            });
+        }
+    }
+}
