dcb9f342ba7d6a439d9718f511f1284c2144bb0c
Maxim Khanin <maksimkhanin.kpi@gmail.com>
2018-04-06 16:05:25 +0300

    HW27

---
+++ CSharpHW/HW27/BookStore/BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookContext db = new BookContext();
+
+        public async Task<ActionResult> Index()
+        {
+            var books = await db.Books.ToListAsync();
+            ViewBag.Books = books;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<string> Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            await Task.Run(() =>
+            {
+                db.Purchases.Add(purchase);
+                db.SaveChanges();
+            });
+            return "Thanks," + purchase.Person + ", for purchase!";
+        }
+    }
+}
---
+++ CSharpHW/HW27/BookStore/BookStore/BookStore/Models/BookContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Book1", Author = "Author1", Price = 100, Id = 001 });
+            db.Books.Add(new Book { Name = "Book2", Author = "Author2", Price = 200, Id = 002 });
+            db.Books.Add(new Book { Name = "Book3", Author = "Author3", Price = 300, Id = 003 });
+
+            base.Seed(db);
+        }
+    }
+}
